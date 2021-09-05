7bf1cc4c6efd505250facd2677eae015ed154a77
ispawel <isapawel@gmail.com>
2021-09-05 21:13:12 +0700

    calcCoffe

--- calcCoffe/ViewController.swift
+++ calcCoffe/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gramCoffe: UITextField!
+   
+    @IBOutlet weak var espressoLabel: UILabel!
+    
+    @IBOutlet weak var milkLatte: UILabel!
+    @IBOutlet weak var latte: UILabel!
+    
+    @IBOutlet weak var milkCapp: UILabel!
+    @IBOutlet weak var capp: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+        @IBAction func button(_ sender: UIButton){
+            view.endEditing(true)
+            
+            let espressoGramm = "\((Int(gramCoffe.text!)!) * 2) g."
+            espressoLabel.text = espressoGramm
+            
+            milkLatte.text = "\(((Int(gramCoffe.text!)!) * 2) * 7 - ((Int(gramCoffe.text!)!) * 2)) g."
+            latte.text = "\(((Int(gramCoffe.text!)!) * 2) * 7) g."
+            milkCapp.text = "\(((Int(gramCoffe.text!)!) * 2) * 4 - ((Int(gramCoffe.text!)!) * 2)) g."
+            capp.text = "\(((Int(gramCoffe.text!)!) * 2) * 4) g."
+    }
+    
 }
-
